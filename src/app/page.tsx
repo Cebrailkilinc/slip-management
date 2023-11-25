23cb65039c020ae6271747e45d5f8470bf1b4a38
Cebrail Kilinc <cebrailyyy57@outlook.com>
2023-11-25 14:29:55 +0300

    Chakra UI has been integrated into the project. The necessary packages have been installed, and a new component named `Providers` has been created to define Chakra UI providers.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import styles from './page.module.css'
+import { Button } from "@chakra-ui/react";
+
 
 export default function Home() {
   return (
     <main>
-      
+      <Button>gabrierl</Button>
     </main>
   )
 }
